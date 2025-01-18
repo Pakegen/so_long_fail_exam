90be7f08e7ff2be721270ce580d309ae46dcec58
Lanila <109435712+Pakegen@users.noreply.github.com>
2025-01-18 16:25:37 +0100

    Add files via upload

---
+++ utils/libft/ft_print_u.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_u.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: quenalla <quenalla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 15:10:03 by quenalla          #+#    #+#             */
+/*   Updated: 2025/01/14 13:14:34 by quenalla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+int	ft_print_u(unsigned int n)
+{
+	char	nbr;
+	int		contador;
+
+	contador = 0;
+	if (n == 0)
+		return (ft_printchar('0'));
+	if (n > 9)
+		contador += ft_print_u(n / 10);
+	nbr = (n % 10) + '0';
+	contador += ft_printchar(nbr);
+	return (contador);
+}
